3f8ac1ba549e05de3f57bf99f274a2df69c52c8e
João <jell_k@hotmail.com>
2025-06-27 10:12:46 -0300

    Deploy teste

--- src/main/java/com/core/config/RestClientConfig.java
+++ src/main/java/com/core/config/RestClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -15,14 +16,16 @@ public class RestClientConfig {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.US_EAST_1) // Ou use uma variável de ambiente
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
     @Bean
     public SqsClient sqsClient() {
         return SqsClient.builder()
-                .region(Region.US_EAST_1) // Ou use uma variável de ambiente
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
